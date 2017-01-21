b7a9705862ea47175bd148086c24d7a0d2c718be
Lucasfrota <frota_lv@hotmail.com>
2017-01-20 23:05:39 -0400

    movimentacao personagem
    movimentaçao do personagem, seguranca que pesquese o personagem e lata
    que sai da plateia ao centroi do palco

---
+++ Assets/lataBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lataBehaviour : MonoBehaviour {
+
+	public Rigidbody2D lata;
+
+	private Vector2 alvo;
+
+	// Use this for initialization
+	void Start () {
+		alvo = new Vector2 (transform.position.x, transform.position.y + 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, alvo,0.03f);
+	}
+}
---
+++ Assets/segurancaBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class segurancaBehaviour : MonoBehaviour {
+
+	public Transform personagem;
+	private Vector2 alvo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		alvo = personagem.transform.position;
+		transform.position = Vector3.MoveTowards(transform.position, alvo,0.04f);
+	}
+}
