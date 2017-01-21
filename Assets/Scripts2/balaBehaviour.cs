eab91485f4deac1ccda4d04db40ffaa64fdd8d54
matheuspb95 <matheuspb95@gmail.com>
2017-01-21 13:13:29 -0400

    Bala script

---
+++ Assets/Scripts2/balaBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class balaBehaviour : MonoBehaviour {
+
+	public Rigidbody2D bala;
+	public GameObject personagem;
+
+	private Vector3 alvo;
+	private Vector3 personagemVec;
+
+	// Use this for initialization
+	void Start () {
+		alvo = Input.mousePosition;
+
+		alvo = Camera.main.ScreenToWorldPoint (alvo);
+		alvo = new Vector3 (alvo.x, alvo.y, -1);
+		personagemVec = personagem.transform.position;
+		alvo = alvo - personagemVec;
+
+		bala.velocity = alvo*3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (transform.position.x.ToString());
+		if(transform.position.x > 10 || transform.position.x < -10 || transform.position.y > 10 || transform.position.y < -10){
+			Destroy (transform.gameObject);
+		}
+	}
+}
